f3c1127283cba249eb2fb5ff0ae118f44d9eb94b
Lerchell <obata.s.ac@gmail.com>
2021-11-26 17:36:16 +0900

    Made views, controllers, models files.

---
+++ Personarily2/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Personarily2
+//
+//  Created by Sae Obata on 2021/11/26.
+//
+
+struct K {
+    struct Segue {
+        static let selectTheme = "SelectThemeSegue"
+        static let history = "HistorySegue"
+        static let ThemeToAnswerQuiz = "QuizSegue"
+    }
+    struct Cells {
+        static let QuizCellNibName = "QuizCell"
+        static let QuizCellIdentifier = "QuizCellIdentifier"
+        static let HistoryCellIdentifier = "HistoryCell"
+        static let SelectQuizCellIdentifier = "SelectQuizCell"
+    }
+}
---
+++ Personarily2/Views/QuizCell.swift
@@ -0,0 +1,23 @@
+//
+//  QuizCell.swift
+//  Personarily2
+//
+//  Created by Sae Obata on 2021/11/26.
+//
+
+import UIKit
+
+class QuizCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
