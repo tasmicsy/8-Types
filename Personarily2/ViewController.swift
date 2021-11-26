9ddc0e1cd35d1edb9826d79c03e17115c08a7a6e
Lerchell <obata.s.ac@gmail.com>
2021-11-26 14:18:18 +0900

    Initial Commit

---
+++ Personarily2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personarily2
+//
+//  Created by Sae Obata on 2021/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
