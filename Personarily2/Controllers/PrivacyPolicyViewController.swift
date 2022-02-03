a736689594f9664057f72a2c02c3c0be71eb6625
Lerchell <obata.s.ac@gmail.com>
2022-02-03 19:41:55 +0900

    App store申請直前、プライバシーポリシー変更

--- Personarily2/Controllers/PrivacyPolicyViewController.swift
+++ Personarily2/Controllers/PrivacyPolicyViewController.swift
@@ -15,21 +15,24 @@ class PrivacyPolicyViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         privacyPolicyText.text = """
-本アプリが提供するプライバシーポリシーについて
+ 8 Types：8項目の性格診断（以下本アプリという）が提供するプライバシーポリシーについて
 
 ・個人情報の収集
-本アプリは、広告配信のためにGoogle AdMob、利用状況解析のためにGoogle Firebaseを使用しています。
+本アプリは、広告配信のためにGoogle AdMob、利用状況解析と質問回答結果収集のためにGoogle Firebaseを使用しています。
 ・広告について
-本アプリでは、広告配信を目的として以下の広告配信事業者がご利用者の情報を自動取得する場合がございます。取得する情報、利用目的、第三者への提供等につきましては、以下の広告配信事業者のアプリケーション・プライバシーポリシーのリンクよりご確認ください。
+本アプリでは、広告配信を目的として以下の広告配信事業者がご利用者の情報を自動取得する場合があります。取得する情報、利用目的、第三者への提供等については、以下の広告配信事業者のアプリケーション・プライバシーポリシーのリンクをご確認ください。
 
 AdMob（Google Inc.）
 http://www.google.com/intl/ja/policies/technologies/ads/
 
-・利用状況解析ついて
+・利用状況解析並びに質問回答結果収集について
 本アプリでは、今後の開発の参考とするため、アプリの利用状況データを収集するツールとしてFirebase(Google Inc.)を使用しており、Firebaseがご利用者の情報を自動取得する場合がございます。取得する情報、利用目的、第三者への提供等につきましては、以下のGoogleプライバシーポリシーのリンクよりご確認ください。
 
 Google プライバシーポリシー
 https://policies.google.com/privacy?hl=ja
+
+Firebase(Google Inc.)
+https://firebase.google.com/support/privacy?hl=ja
 """
     }
     
